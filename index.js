255d1cd3a903134503ef083f6063725afa2b52e9
Валерійка <kudravka13@gmail.com>
2017-10-09 16:26:22 +0300

    Fixed the code style and work with array = [undefined, '1.2a', 2, 1]

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-var array = [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1];
-var min_num = array[0],max_num = array[0], sum = 0,counter, type_num ;
+var array = [undefined, '1.2a', 2, 1];
+var min_num,
+    max_num,
+    sum = 0,
+    counter,
+    type_num;
 
-for(counter = 0; counter < array.length; counter++){
-
+for (counter = 0; counter < array.length; counter++) {
     type_num = typeof array[counter];
-    if(( type_num === 'number' )&&(!isNaN(array[counter]))){
-            sum += array[counter];
-
-            if (min_num > array[counter]){
-               min_num =  array[counter];
-            }
-        if (max_num < array[counter]){
-            max_num =  array[counter];
+    if ((type_num === 'number') && (!isNaN(array[counter]))) {
+        min_num = array[counter];
+        max_num = array[counter];
+        break;
+    }
+}
+for (counter = 0; counter < array.length; counter++) {
+    type_num = typeof array[counter];
+    if ((type_num === 'number') && (!isNaN(array[counter]))) {
+        sum += array[counter];
+        if (min_num > array[counter]) {
+            min_num = array[counter];
+        }
+        if (max_num < array[counter]) {
+            max_num = array[counter];
         }
     }
 }
